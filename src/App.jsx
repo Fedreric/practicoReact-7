cca2b68e018832600160b9739b0448486e182e11
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-05-20 17:06:58 -0300

    Se agrega el componente ListGroup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import EmpleadoList from './components/EmpleadoList';
 function App() {
 
   return (
     <>
-     
+     <EmpleadoList></EmpleadoList>
     </>
   )
 }
