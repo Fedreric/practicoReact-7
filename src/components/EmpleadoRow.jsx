a9d68a1288306aafd37d9631396c540464cf8f6f
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-05-22 16:18:18 -0300

    se recorre el array de empleados utilizando map y se muestra correctamente a los empleados

--- src/components/EmpleadoRow.jsx
+++ src/components/EmpleadoRow.jsx
@@ -2,18 +2,16 @@ import { ListGroup, Badge } from "react-bootstrap";
 import EmpleadoAvatar from "./EmpleadoAvatar";
 
 const EmpleadoRow = ({ empleado }) => {
-  const listaEmpleados = empleado;
-
   return (
     <ListGroup.Item className="my-1">
       <article className="row align-items-center">
         <div className="col-4 col-md-2">
-          <EmpleadoAvatar avatar={listaEmpleados[0].pic} />
+          <EmpleadoAvatar avatar={empleado.pic} />
         </div>
         <div className="col-8 col-md-10">
-          <p>{listaEmpleados[0].fullName}</p>
-          <strong>{listaEmpleados[0].title}</strong>
-          <Badge className="mx-1">{listaEmpleados[0].department}</Badge>
+          <p>{empleado.fullName}</p>
+          <strong>{empleado.title}</strong>
+          <Badge className="mx-1">{empleado.department}</Badge>
         </div>
       </article>
     </ListGroup.Item>
